ed6c4a3ee225a5019d0bb90529d1de8ad73bf8b3
xiurobert <xiurobert@gmail.com>
2021-01-09 09:56:56 +0800

    death

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  SafeAreaView,
+} from 'react-native';
+import AppRoot from './actual_src/approot.js';
+import UserInfoPage from './actual_src/pages/UserInfoPage.js';
+import LoadingPage from './actual_src/pages/LoadingPage.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Styles from './actual_src/stylesheet.js';
+import Menu from './actual_src/menu.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      userinfogained: 'loading'
+    }
+  };
+
+  conditionalrender = () => {
+    if (this.state.userinfogained == 'true'){
+      return <AppRoot />
+    } else if (this.state.userinfogained == 'false') {
+      return <UserInfoPage CUDFun={this.checkUserData} />
+    } else {
+      return <LoadingPage />
+    }
+  }
+
+  checkUserData = async () => {
+    try{
+      const UD = await AsyncStorage.getItem('weight');
+      if (UD == null){
+        this.setState({userinfogained: 'false'})
+      } else {
+        this.setState({userinfogained: 'true'})
+      }
+    } catch {
+      console.log('ASYNC FAILED!!!')
+    }
+  }
+
+  componentDidMount(){
+    this.checkUserData();
+  }
+
+  render(){
+    return(
+      <SafeAreaView style={{flex:1}}>
+        {this.conditionalrender()}
+      </SafeAreaView>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
